966a8f144aba862e72081a27fa6f0d80bb8c8d08
vaibhav <bhardwajvaibhav0210@gmail.com>
2026-03-16 00:41:34 +0530

    fix worker import and docker setup

--- worker/worker.py
+++ worker/worker.py
@@ -1,9 +1,27 @@
-import redis
-from rq import Worker
+import sys
 import os
+import redis
+
+# Make sure Python can find the app module
+sys.path.append("/app")
+
+# IMPORTANT: import the task so RQ can register it
+from app.tasks import process_job
+
+from rq import Worker, Queue, Connection
+
 
 redis_host = os.getenv("REDIS_HOST", "redis")
-redis_conn = redis.Redis(host=redis_host, port=6379, db=0)
 
-worker = Worker(["default"], connection=redis_conn)
-worker.work()
+redis_conn = redis.Redis(
+    host=redis_host,
+    port=6379,
+    db=0
+)
+
+listen = ["default"]
+
+if __name__ == "__main__":
+    with Connection(redis_conn):
+        worker = Worker(list(map(Queue, listen)))
+        worker.work()
